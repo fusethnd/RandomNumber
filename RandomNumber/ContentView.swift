b7c6e4908dad2cfaf25bf574150eef29eb0fced5
fusethnd <fusethnd@gmail.com>
2024-01-19 16:02:50 +0700

    Initial Commit

---
+++ RandomNumber/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomNumber
+//
+//  Created by Thanadon Boontawee on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomNumber/RandomNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberApp.swift
+//  RandomNumber
+//
+//  Created by Thanadon Boontawee on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
