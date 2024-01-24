d4fc40e3056e6f7b63a75c334d3dde87feccf190
fusethnd <fusethnd@gmail.com>
2024-01-24 15:19:34 +0700

    add round frame and fill color in frame

--- RandomNumber/ContentView.swift
+++ RandomNumber/ContentView.swift
@@ -28,60 +28,90 @@ struct ContentView: View {
             
             VStack {
                 ZStack {
+                    Image("Group-title")
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
                     Text("Guess me\nwhat I am?")
+                        .foregroundColor(.white)
+                        .font(Font.custom("Fredoka-Regular", size: 25))
                 }
                 // Square with Border
-                Rectangle()
-                    .stroke(Color(hex: 0xA85116), lineWidth: 13)
-                    // Customize the border color and width
-                    .frame(width: 327, height: 549.8) //549.8)
-                    // Set the width and height of the square
-                    .overlay( // Text("Hello, world")
-                        VStack{
-                            Text("Round")
-                            ZStack {
-                                Text("\(game.count)")
-                            }
+                ZStack {
+                    Rectangle()
+                        .fill(Color(hex: 0xFFD524))
+                        .frame(width: 327, height: 549.8)
+                    Rectangle()
+                        // .fill(Color(hex: 0xFFD524))
+                        .stroke(Color(hex: 0xA85116), lineWidth: 13)
+                        // Customize the border color and width
+                        .frame(width: 327, height: 549.8)
+                        // Set the width and height of the square
+                        .overlay( // Text("Hello, world")
+                            VStack{
+                                Text("Round")
+                                    .foregroundColor(Color(hex: 0xA85116))
+                                    .font(Font.custom("Fredoka-Regular", size: 20))
+                                ZStack {
+                                    Rectangle()
+                                        .fill(Color(.white))
+                                        // Customize the border color and width
+                                        .frame(width: 278.9, height: 65.6)
+                                    Rectangle()
+                                        .stroke(Color(hex: 0xA85116), lineWidth: 8)
+                                        // Customize the border color and width
+                                        .frame(width: 278.9, height: 65.6)
+                                        // Set the width and height of the square
+                                        .overlay(
+                                            Text("\(game.count)")
+                                                .font(Font.custom("Fredoka-Regular", size: 20))
+                                        )
+                                }
 
-                            ZStack {
-                                // Background Image
-                                Image("fruit")
-                                    .aspectRatio(contentMode: .fill)
-                                    .edgesIgnoringSafeArea(.all)
+                                ZStack {
+                                    // Background Image
+                                    Image("fruit")
+                                        .aspectRatio(contentMode: .fill)
+                                        .edgesIgnoringSafeArea(.all)
 
-                                // Text on Top
-                                Text(String(Game.toint(val: guess)))
-                                    .foregroundColor(.white)
-                                    .font(Font.custom("Fredoka-Regular", size: 50))
-                                    .padding()
+                                    // Text on Top
+                                    Text(String(Game.toint(val: guess)))
+                                        .foregroundColor(.white)
+                                        .font(Font.custom("Fredoka-Regular", size: 50))
+                                        .padding()
+                                }
+                                HStack {
+                                    Text("0")
+                                    Slider(value: $guess)
+                                        .frame(width: 200)
+                                        .accentColor(Color(hex: 0xA85116))
+                                    Text("100")
+                                }
                             }
-                            HStack {
-                                Text("0")
-                                Slider(value: $guess)
-                                    .frame(width: 200)
-                                    .accentColor(Color(hex: 0xA85116))
-                                Text("100")
+                        )
+                }
+               
+                
+                ZStack {
+                    Image("Group-button")
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+
+                    Button("HIT ME!") {
+                        showResult = true
+                        game.check(guess: guess)
+                    }.padding()
+                    .alert(isPresented: $showResult) {
+                        Alert(
+                            title: Text("Your result"),
+                            message: Text(game.text),
+                            dismissButton: .default(Text("OK")) {
+                                if game.correct {
+                                    guess = 0
+                                    game.startNewGame()
+                                }
                             }
-                        }
-                    )
-                
-                Button("HIT ME!") {
-                    showResult = true
-                    game.check(guess: guess)
-                }.padding()
-                
-                
-                .alert(isPresented: $showResult) {
-                    Alert(
-                        title: Text("Your result"),
-                        message: Text(game.text),
-                        dismissButton: .default(Text("OK")) {
-                            if game.correct {
-                                guess = 0
-                                game.startNewGame()
-                            }
-                        }
-                    )
+                        )
+                    }
                 }
             }
         }
